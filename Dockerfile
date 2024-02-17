456eca7a6ba3c8fdf2e620b49f8a1c236bd41ad6
Abhishek Gupta <abhig0209@gmail.com>
2024-02-17 15:41:47 +0530

    first commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+# COPY calculator.sh /app/calculator.sh
+COPY Calculator.java /app/Calculator.java
+# CMD sed -i 's/\r$//' /app/calculator.sh
---
+++ src/main/java/org/calculator/Main.java
@@ -0,0 +1,8 @@
+package org.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        cal.main();
+    }
+}
